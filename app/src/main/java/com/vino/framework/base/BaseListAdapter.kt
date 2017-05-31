a4903af663814d6564f339eee07b0d6d43eb3bba
vinomvp <13411129737@163.com>
2017-05-31 21:50:31 +0800

    修改了baseAdapter

--- app/src/main/java/com/vino/framework/base/BaseListAdapter.kt
+++ app/src/main/java/com/vino/framework/base/BaseListAdapter.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import java.lang.reflect.ParameterizedType
 
 /**
  * @项目名: Framework
@@ -12,7 +13,7 @@ import android.widget.BaseAdapter
  * @创建时间: 2017/5/24 20:43
  * @描述: listView adapter的基类
  */
-abstract class BaseListAdapter<T>(val list: List<T>, val activity: Activity) : BaseAdapter() {
+abstract class BaseListAdapter<T, out E : BaseHolder<T>>(val list: List<T>, val activity: Activity) : BaseAdapter() {
 
     override fun getCount(): Int {
         return list.size
@@ -27,23 +28,25 @@ abstract class BaseListAdapter<T>(val list: List<T>, val activity: Activity) : B
     }
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        var viewHolder: BaseHolder<T>
-        var view: View
+        val viewHolder: E
+        val view: View
         if (convertView === null) {
-            viewHolder = getHolder(activity, parent)
+            // 通过泛型创建presenter对象
+            val arguments = (javaClass.genericSuperclass as ParameterizedType).actualTypeArguments
+            val clazz = arguments[1] as Class<E>
+            val cons = clazz.getConstructor(javaClass, Activity::class.java, ViewGroup::class.java)
+            viewHolder = cons.newInstance(activity, parent)
             view = viewHolder.getRootView(parent)
             view.tag = viewHolder
         } else {
             view = convertView
             @Suppress("UNCHECKED_CAST")
-            viewHolder = view.tag as BaseHolder<T>
+            viewHolder = view.tag as E
         }
 
         viewHolder.setData(list[position], position)
 
         return view
     }
-
-    abstract fun getHolder(activity: Activity, parent: ViewGroup?): BaseHolder<T>
 
 }
--- app/src/main/java/com/vino/framework/base/BaseRecyclerAdapter.kt
+++ app/src/main/java/com/vino/framework/base/BaseRecyclerAdapter.kt
@@ -2,9 +2,11 @@ package com.vino.framework.base
 
 import android.app.Activity
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.vino.framework.listener.OnItemClickListener
+import java.lang.reflect.ParameterizedType
 
 /**
  * @项目名: Framework
@@ -13,7 +15,8 @@ import com.vino.framework.listener.OnItemClickListener
  * @创建时间: 2017/5/24 21:24
  * @描述: recyclerView 的baseAdapter
  */
-abstract class BaseRecyclerAdapter<T>(val list: List<T>, val activity: Activity) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+abstract class BaseRecyclerAdapter<T, out E : BaseRecyclerHolder<T>>(val list: List<T>, val activity: Activity)
+    : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     var onItemClickListener: OnItemClickListener? = null
 
@@ -22,16 +25,25 @@ abstract class BaseRecyclerAdapter<T>(val list: List<T>, val activity: Activity)
         (holder as BaseRecyclerHolder<T>).setData(list[position], position)
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): BaseRecyclerHolder<T> {
-        return getHolder(getRootView(parent), activity, onItemClickListener)
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): E {
+        // 通过泛型创建presenter对象
+        val arguments = (javaClass.genericSuperclass as ParameterizedType).actualTypeArguments
+        val clazz = arguments[1] as Class<E>
+        val cons = clazz.getConstructor(javaClass, View::class.java, Activity::class.java, OnItemClickListener::class.java)
+        return cons.newInstance(this,getRootView(parent), activity, onItemClickListener)
     }
-
-    abstract fun getHolder(rootView: View, activity: Activity, onItemClickListener: OnItemClickListener?): BaseRecyclerHolder<T>
 
     override fun getItemCount(): Int {
         return list.size
     }
 
-    abstract fun getRootView(parent: ViewGroup?): View
+    fun getRootView(parent: ViewGroup?): View {
+        return LayoutInflater.from(activity).inflate(getLayoutId(), parent, false)
+    }
+
+    /**
+     * 获取布局id
+     */
+    abstract fun getLayoutId(): Int
 
 }
